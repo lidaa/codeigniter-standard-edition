30702a5ce6249d94ae3e24df7783d75a7c1559ac
Adil OUkHA <aa_dil@hotmail.fr>
2016-04-19 17:36:02 +0100

    Cs_Fixer Command

--- bin/cise/Cs_Fixer.php
+++ bin/cise/Cs_Fixer.php
@@ -5,23 +5,33 @@ namespace Cise;
 use Composer\Script\Event;
 
 /**
- * Description of Clear_Logs
+ * Cs_Fixer Command
  *
  * @author Lidaa <aa_dil@hotmail.fr>
  */
-class Cs_Fixer extends Command {
-    
+class Cs_Fixer extends Command
+{
+
     /**
      * run
      * 
      * @param Event $event
      */
-    public static function run(Event $event = null) {
+    public static function run(Event $event = null)
+    {
         $io = self::getIO($event);
-     
-        $vendor_path = dirname(dirname(dirname(BASEPATH)));
-        $php_cs_fixer =  sprintf('%s/bin/php-cs-fixer', $vendor_path);
 
-        exec($php_cs_fixer . ' ' . self::getArgv(true));
+        $vendor_path = dirname(dirname(dirname(BASEPATH)));
+        $php_cs_fixer = sprintf('%s/bin/php-cs-fixer', $vendor_path);
+
+        $cmd = sprintf('%s fix --config-file %s/.php_cs', $php_cs_fixer, dirname(APPPATH));
+
+        $io->write(sprintf('<info>Runing %s</info>', $cmd));
+
+        $rep = system($cmd);
+
+        if ($rep) {
+            $io->write('<info>Successfully fixed.</info>');
+        }
     }
 }
